f143acf40e60a7e8b37c191cf9b29e04c192660a
Yernur Shapkat <113994667+Fruzzy07@users.noreply.github.com>
2024-03-13 21:00:00 +0500

    Add files via upload

---
+++ lab6/src/app/album-detail-photos/service/album-detail-photos-api.service.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed } from '@angular/core/testing';
+
+describe('AlbumDetailPhotosAPIService', () => {
+  let service: AlbumDetailAPIService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AlbumDetailPhotosAPIService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
